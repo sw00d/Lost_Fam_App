f4866365b1467f4f53d10456f71cca3837a66e83
Samuelote <samote.wood@gmail.com>
2018-02-27 19:29:55 -0700

    working camera

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default class App extends React.Component {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    width: '100%',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
--- components/camera.js
+++ components/camera.js
@@ -1,48 +1,92 @@
-'use strict';
-import React, { Component } from 'react';
-import {
-  AppRegistry,
-  Dimensions,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View,
-  Button,
-  Image
-} from 'react-native';
-import { ImagePicker } from 'expo';
+import React from 'react';
+import { Text, View, TouchableOpacity, Dimensions, StyleSheet } from 'react-native';
+import { Camera, Permissions } from 'expo';
 
 
-export default class ImagePickerExample extends React.Component {
+export default class CameraExample extends React.Component {
   state = {
-    image: null,
+    hasCameraPermission: true,
+    type: Camera.Constants.Type.back,
   };
 
-  render() {
-    let { image } = this.state;
-
-    return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          title="Pick an image from camera roll"
-          onPress={this._pickImage}
-        />
-        {image &&
-          <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />}
-      </View>
-    );
+  async componentWillMount() {
+    console.log("pomc")
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    // this.setState({ hasCameraPermission: status === 'granted' });
   }
 
-  _pickImage = async () => {
-    let result = await ImagePicker.launchCameraAsync({
-      allowsEditing: false,
-      aspect: [4, 3],
-    });
+  typeConfig() {
+    console.log('dumpshit')
 
-    console.log(result);
+  }
 
-    if (!result.cancelled) {
-      this.setState({ image: result.uri });
+  render() {
+    const { hasCameraPermission } = this.state;
+    const type = this.state.type;
+    const {height, width} = Dimensions.get('window');
+
+
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else if (hasCameraPermission === true){
+      return (
+        <View>
+          <View style={styles.topBanner}></View>
+          <Camera style={{ width: width, height: height }} type= { type }>
+          <View
+              style={{
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'row',
+              }}>
+              <View
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                 this.setState({
+                   type: this.state.type === Camera.Constants.Type.back
+                     ? Camera.Constants.Type.front
+                     : Camera.Constants.Type.back,
+                 });
+              }}>
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </View>
+          </View>
+          </Camera>
+          <View style={styles.bottomBanner}></View>
+        </View>
+      );
     }
-  };
+  }
 }
+
+const {height, width} = Dimensions.get('window');
+const styles = StyleSheet.create({
+
+  topBanner: {
+    height: height/6,
+    width: width,
+    backgroundColor: '#E9C28B',
+    alignItems: 'center',
+    justifyContent: 'center',
+    position: 'absolute',
+    top: 0,
+  },
+  bottomBanner: {
+    position: 'absolute',
+    bottom: 0,
+    height: height/6,
+    width: width,
+    backgroundColor: '#E9C28B',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
