60657783fc66875af011ce986bac5eebc801ebd2
mrchadparkour <cplattkuhn@gmail.com>
2018-03-17 19:49:13 -0600

    changes for register

--- src/components/signUp/signUpView.js
+++ src/components/signUp/signUpView.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import {ScrollView, Text, View, TouchableOpacity} from 'react-native';
 import styles from './styles';
-import { Container, Header, Content, Form, Button, Left, Body, Right, Icon, Title, } from 'native-base';
 import { renderField } from './renderField';
 import {Field} from 'redux-form';
 import { canNavToNext } from '../../utils';
@@ -13,8 +12,10 @@ export default class SignUpView extends Component {
 
   submit() {
     const { register, validate } = this.props;
-    if (!register) return;
-    if (!canNavToNext(register, validate)) return;
+    // if (!register) return;
+    // console.log("is reg ister");
+    // console.log(register);
+    // if (!canNavToNext(register, validate)) return;
     this.props.createUser(register);
   }
 
@@ -41,17 +42,10 @@ export default class SignUpView extends Component {
               <Field name="password" label={"Password"} component={renderField} />
               <Field name="confirmPass" label={"Confirm Password"} component={renderField} />
           </Form>
-<<<<<<< HEAD
           <Button style={styles.submitBtn} onPress={() => this.submit()}>
             <Text style={styles.btnFont}>Sign Up</Text>
           </Button>
-=======
->>>>>>> e81887cad274fd16c6f3d9c48afc42e91c29e91d
         </Content>
-        <Button style={styles.submitBtn} onPress={this.submit.bind(this)}>
-          <Text style={styles.btnFont}>Sign Up</Text>
-        </Button>
-
       </View>
     );
   }
